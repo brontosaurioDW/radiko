8e89d5a70d65c7c9aa57ee25bcc13ef4abbe87e1
MrJouts <30276816+MrJouts@users.noreply.github.com>
2018-06-25 21:51:01 -0300

    se genera el archivo mail_example.html para crear el newsletter

--- landing/send.php
+++ landing/send.php
@@ -3,44 +3,56 @@
 use PHPMailer\PHPMailer\PHPMailer;
 
 function validateEmail($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
+  return filter_var($email, FILTER_VALIDATE_EMAIL);
 }   
 
 if (!empty($_POST) && 
-    !empty($_POST['email']) && 
-    validateEmail($_POST['email']))
+  !empty($_POST['email']) && 
+  validateEmail($_POST['email']))
 {
-    require_once('vendor/autoload.php');
+  require_once('vendor/autoload.php');
 
 
-    $mail = new PHPMailer;
+  $mail = new PHPMailer;
 
-    $mail->From = "info@radiko.com";
-    $mail->FromName = "Radiko";
+  $mail->From = "info@radiko.com";
+  $mail->FromName = "Radiko";
 
-    $mail->addAddress("es.hotes@gmail.com", "Emiliano");
+  $mail->addAddress("es.hotes@gmail.com", "Emiliano");
 
-    $mail->addReplyTo($_POST['email']);
+  $mail->addReplyTo($_POST['email']);
 
-    $mail->isHTML(true);
+  $mail->isHTML(true);
 
-    $mail->Subject = "Nuevo usuario registrado en Newsletter";
-    $mail->Body = "";
-    $mail->Body .= "<h3>Mensaje enviado desde Radiko</h3>";
-    $mail->Body .= "<b>Email: </b>".$_POST['email']."<br>";
+  $mail->Subject = "Nuevo usuario registrado en Newsletter";
+  $mail->Body = "";
+  $mail->Body .= "<h3>Mensaje enviado desde Radiko</h3>";
+  $mail->Body .= "<b>Email: </b>".$_POST['email']."<br>";
 
-    if(!$mail->send()) 
-    {
-        error_log("Error al enviar el email: " . $mail->ErrorInfo);
-        echo 0;
-    } 
-    else 
-    {
-        echo 1;
-    }
+  if(!$mail->send()) 
+  {
+    error_log("Error al enviar el email: " . $mail->ErrorInfo);
+    echo 0;
+  } 
+  else 
+  {
+
+    $mail->ClearAddresses();
+    $mail->AddAddress($_POST['email']);
+    $mail->Body = file_get_contents('mail_example.html') ;
+    $mail->Send();
+
+    echo 1;
+
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  }
 }
 else
 {
-    echo -1;
+  echo -1;
 }
+
+
+
+
 ?>
