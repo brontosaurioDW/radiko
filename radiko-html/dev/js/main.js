c3c5bffe5bc5124b3b333ec994da8074b9642641
florsepulveda <florenciasepulveda.26@gmail.com>
2018-07-25 02:30:59 -0300

    home-otra actualizacion

--- radiko-html/dev/js/main.js
+++ radiko-html/dev/js/main.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
 	var headerHome = $('.is-home');
 
 	$(window).scroll(function(){
-        if ($(window).scrollTop() > 300){
+        if ($(window).scrollTop() > 54){
             $(headerHome).addClass('sticky');
+            $(headerHome).addClass('sticky-header');
         } else {
-        	$(headerHome).removeClass('sticky');
+            $(headerHome).removeClass('sticky');
+        	$(headerHome).removeClass('sticky-header');
         }
     });
 
