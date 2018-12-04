b82785fd961cb60d5c333cbd188d8dd8285df47d
lauramitono <mlauramitono@gmail.com>
2018-12-03 22:06:21 -0300

    Se actualiza carpeta para grabar dvd

--- Radiko-DW6ATN-2018-PF/03_Demo/radikoLaravel/resources/views/home/suscribite-banner.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="why-radiko align-items-center d-none d-lg-flex bg-trama-b">
-    <div class="container text-center">
-        <div class="title">
-            <h2>Unite a Radiko y sumate a una vida más sana</h2>
-        </div>
-        <a href="{{ url('/registro') }}" class="btn-primary btn btn-medium"> Registrate </a>
-    </div>
-</section>
